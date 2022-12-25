d59c1b0ba81c31862b2beb842bdd2646bfec22f9
Aleksey <Alecsey913@mail.ru>
2022-12-25 17:16:26 +0300

    Update docker-compose

--- serviceone/Controllers/HomeController.cs
+++ serviceone/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ public class HomeController : ControllerBase
     public HomeController()
     {
         _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://servicetwo:80/");
+        _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("SERVICE_TWO"));
         _client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
     }
